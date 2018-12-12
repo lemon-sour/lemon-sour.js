44cf1602b2333440424526bf949c67e6ac29c4c2
hisasann <hisasann25@gmail.com>
2018-12-13 05:56:11 +0900

    Add ora and thanks in package.json

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
 'use strict';
 
-// beer!!
-console.log('🍺!!');
+const ora = require('ora');
+
+const HEARTS_SPINNER = {
+  'interval': 100,
+  'frames': [
+    '💛 ',
+    '💙 ',
+    '💜 ',
+    '💚 '
+  ]
+};
+
+const spinner = ora({
+  spinner: HEARTS_SPINNER,
+  text: 'start'
+}).start();
+
+// spinner.fail('failed');
+
+spinner.color = 'yellow';
+
+setTimeout(() => {
+  spinner.text = 'loading...\n';
+}, 1000);
+
+setTimeout(() => {
+  // beer!!
+  console.log('🍺\n');
+  spinner.succeed('thanks!');
+}, 4000);
