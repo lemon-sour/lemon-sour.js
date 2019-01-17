e412bfb04ca5b2417836a6e08373a8b5a27312f6
hisasann <hisasann25@gmail.com>
2019-01-17 17:42:23 +0900

    test(cli): add CliArgsInterface

--- packages/cli/__tests__/index.test.ts
+++ packages/cli/__tests__/index.test.ts
@@ -1,10 +1,11 @@
 process.argv.push('-x', '1');
 
 import { cli } from '../src/index';
+import { CliArgsInterface } from '../src/interface/cli-args-interface';
 
 describe('index', () => {
   test('run index.', () => {
-    const args = cli();
+    const args: CliArgsInterface = cli();
     expect(args.x).toBe(1);
   });
 });
