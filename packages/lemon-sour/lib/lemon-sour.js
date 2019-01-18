63d33d3a1e7290e6dc6257510fe93f2e862e8549
hisasann <hisasann25@gmail.com>
2019-01-18 13:29:24 +0900

    feature: add to load yml file with js-yaml npm module

--- packages/lemon-sour/lib/lemon-sour.js
+++ packages/lemon-sour/lib/lemon-sour.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const yaml = require("js-yaml");
+let doc = yaml.safeLoad(fs.readFileSync('../../example/app_a/index.yml', 'utf8'));
+console.log(JSON.stringify(doc, undefined, 2));
 class LemonSour {
     constructor() { }
     run(args) {
--- packages/lemon-sour/src/lemon-sour.ts
+++ packages/lemon-sour/src/lemon-sour.ts
@@ -1,5 +1,12 @@
 import { CliArgsInterface } from '@lemon-sour/cli';
 import * as _ from 'lodash';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
+
+let doc = yaml.safeLoad(
+  fs.readFileSync('../../example/app_a/index.yml', 'utf8'),
+);
+console.log(JSON.stringify(doc, undefined, 2));
 
 class LemonSour {
   constructor() {}
