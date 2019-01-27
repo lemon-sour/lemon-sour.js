fe824ea20a44f40054592060ece8101b6ae1f933
hisasann <hisasann25@gmail.com>
2019-01-27 14:56:59 +0900

    feature: add TODO to lemon-sour.js

--- packages/lemon-sour/lib/lemon-sour.js
+++ packages/lemon-sour/lib/lemon-sour.js
@@ -22,6 +22,7 @@ class LemonSour {
     run(args) {
         return __awaiter(this, void 0, void 0, function* () {
             // TODO オフラインか判定うまくできるならここでやりたい
+            // TODO args.yml がない場合の処理をここでやりたい
             // Load yml file
             const doc = yaml_loader_1.yamlLoader(args.yml);
             // console.log(JSON.stringify(doc, undefined, 2));
