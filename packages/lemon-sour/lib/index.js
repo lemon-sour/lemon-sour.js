2532e486348e0e000206dcda5338bb78e49e0908
hisasann <hisasann25@gmail.com>
2019-01-31 05:05:17 +0900

    feat: add sentry to lemon-sour entrypoint

--- packages/lemon-sour/lib/index.js
+++ packages/lemon-sour/lib/index.js
@@ -3,9 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const cli_1 = require("@lemon-sour/cli");
 const lemon_sour_1 = require("./lemon-sour");
 /**
+ * Sentry
+ * https://sentry.io/lemon-sour/nodejs/
+ */
+const Sentry = require('@sentry/node');
+Sentry.init({ dsn: 'https://0120a1efd0a643c7be7abdf8b43f2959@sentry.io/1373564' });
+/**
  * bootstrap
  */
 const args = cli_1.cli();
-lemon_sour_1.lemonSour.run(args).catch(e => {
-    console.error(e);
+lemon_sour_1.lemonSour.run(args).catch(err => {
+    console.error(err);
+    Sentry.captureException(err);
 });
--- packages/lemon-sour/src/index.ts
+++ packages/lemon-sour/src/index.ts
@@ -4,9 +4,19 @@ import { CliArgsInterface } from '@lemon-sour/cli';
 import { lemonSour } from './lemon-sour';
 
 /**
+ * Sentry
+ * https://sentry.io/lemon-sour/nodejs/
+ */
+const Sentry = require('@sentry/node');
+Sentry.init({
+  dsn: 'https://0120a1efd0a643c7be7abdf8b43f2959@sentry.io/1373564',
+});
+
+/**
  * bootstrap
  */
 const args: CliArgsInterface = cli();
-lemonSour.run(args).catch(e => {
-  console.error(e);
+lemonSour.run(args).catch(err => {
+  console.error(err);
+  Sentry.captureException(err);
 });
