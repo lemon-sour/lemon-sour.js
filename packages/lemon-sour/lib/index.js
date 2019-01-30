9e02863c697b2acac5abe104b56db7d87798e9a0
hisasann <hisasann25@gmail.com>
2019-01-31 05:22:25 +0900

    fix: lint:format

--- packages/lemon-sour/lib/index.js
+++ packages/lemon-sour/lib/index.js
@@ -7,7 +7,9 @@ const lemon_sour_1 = require("./lemon-sour");
  * https://sentry.io/lemon-sour/nodejs/
  */
 const Sentry = require('@sentry/node');
-Sentry.init({ dsn: 'https://0120a1efd0a643c7be7abdf8b43f2959@sentry.io/1373564' });
+Sentry.init({
+    dsn: 'https://0120a1efd0a643c7be7abdf8b43f2959@sentry.io/1373564',
+});
 /**
  * bootstrap
  */
