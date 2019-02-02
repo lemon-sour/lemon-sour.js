efe89224594327000d7bffe861d4fbc8bb0bbe36
hisasann <hisasann25@gmail.com>
2019-02-02 18:19:35 +0900

    fix(lemon-sourt): remove unuse npm module

--- packages/lemon-sour/src/index.ts
+++ packages/lemon-sour/src/index.ts
@@ -1,4 +1,3 @@
-import * as childProcess from 'child_process';
 import { cli } from '@lemon-sour/cli';
 import { CliArgsInterface } from '@lemon-sour/cli';
 import { lemonSour } from './lemon-sour';
