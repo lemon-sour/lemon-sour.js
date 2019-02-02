e05340181ae23c870c6d8dd1e225fa98e79a70a8
hisasann <hisasann25@gmail.com>
2019-02-03 01:08:03 +0900

    refactor(lemon-sour): types/index.d.ts

--- packages/json-storage/types/index.d.ts
+++ packages/json-storage/types/index.d.ts
@@ -1,2 +1,2 @@
-export function setJson(jsonKeyName: string, obj: any): Promise<boolean> ;
-export function getJson(jsonKeyName: string): Promise<object | null> ;
+export function setJson(jsonKeyName: string, obj: any): Promise<boolean>;
+export function getJson(jsonKeyName: string): Promise<object | null>;
