57b89c5a7bf75a92cd899ce3defc6a8e31c40077
hisasann <hisasann25@gmail.com>
2019-02-07 06:17:19 +0900

    refactor

--- packages/lemon-sour/src/utils/judgment-online.ts
+++ packages/lemon-sour/src/utils/judgment-online.ts
@@ -4,7 +4,7 @@ import C from '../common/constants';
 
 /**
  * オフライン・オンラインの判定をする関数
- * @param yml
+ * @param env
  */
 const judgmentOnLine = async (env: EnvInterface) => {
   return new Promise<boolean>(
@@ -13,8 +13,8 @@ const judgmentOnLine = async (env: EnvInterface) => {
       reject: (err: boolean) => void,
     ) => {
       // オフラインかどうかの判定
-      // https://stackoverflow.com/questions/5725430/http-test-server-accepting-get-post-requests
       if (env.envName !== 'dev') {
+        // https://stackoverflow.com/questions/5725430/http-test-server-accepting-get-post-requests
         const response = await axios.get(C.offLineJudgmentHttpUrl);
         if (response.status !== C.HTTP_OK) {
           // offline
