c350368579b93f0c2ce74a91ff8991c544fb091b
hisasann <hisasann25@gmail.com>
2019-02-10 08:31:12 +0900

    feat(lemon-sour): add file-downloader

---
+++ packages/lemon-sour/lib/utils/file-downloader.js
@@ -0,0 +1,74 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// https://stackoverflow.com/questions/7323932/how-do-you-follow-an-http-redirect-in-node-js
+// https://github.com/follow-redirects/follow-redirects
+// import * as http from 'http';
+// import * as https from 'https';
+const http = require('follow-redirects').http;
+const https = require('follow-redirects').https;
+const URL = require("url");
+const fs = require("fs");
+const constants_1 = require("../common/constants");
+function download(fileUrl, distPath, progress, callback) {
+    return new Promise((resolve, reject) => {
+        let timeout = 10000;
+        let file = fs.createWriteStream(distPath);
+        const parsedUrl = URL.parse(fileUrl);
+        let httpObject;
+        if (parsedUrl.protocol === 'https:') {
+            httpObject = https;
+        }
+        else {
+            httpObject = http;
+        }
+        let request = httpObject.get(fileUrl).on('response', (res) => {
+            if (res.statusCode !== constants_1.default.HTTP_OK) {
+                clearTimeout(timeoutId);
+                reject(new Error(res.statusMessage));
+                return;
+            }
+            let len = parseInt(res.headers['content-length'], 10);
+            let downloaded = 0;
+            res
+                .on('data', (chunk) => {
+                file.write(chunk);
+                downloaded += chunk.length;
+                // console.log('Downloading ' + (100.0 * downloaded / len).toFixed(2) + '% ' + downloaded + ' bytes');
+                progress(((100.0 * downloaded) / len).toFixed(0), downloaded);
+                // reset timeout
+                clearTimeout(timeoutId);
+                timeoutId = setTimeout(fn, timeout);
+            })
+                .on('end', () => {
+                // clear timeout
+                clearTimeout(timeoutId);
+                file.end();
+                console.log('downloaded to: ' + distPath);
+                resolve();
+                callback();
+            })
+                .on('error', (err) => {
+                // clear timeout
+                console.log('res.on(error): ' + err.message);
+                clearTimeout(timeoutId);
+                reject(err);
+            });
+        });
+        // error handler
+        request.on('error', (err) => {
+            console.log('request.on(error): ' + err.message);
+            clearTimeout(timeoutId);
+            reject(new Error(err.message));
+        });
+        // generate timeout handler
+        let fn = timeoutWrapper(request);
+        // set initial timeout
+        let timeoutId = setTimeout(fn, timeout);
+    });
+}
+function timeoutWrapper(req) {
+    return () => {
+        req.abort();
+    };
+}
+exports.default = download;
---
+++ packages/lemon-sour/src/utils/file-downloader.ts
@@ -0,0 +1,90 @@
+// https://stackoverflow.com/questions/7323932/how-do-you-follow-an-http-redirect-in-node-js
+// https://github.com/follow-redirects/follow-redirects
+// import * as http from 'http';
+// import * as https from 'https';
+const http = require('follow-redirects').http;
+const https = require('follow-redirects').https;
+import * as URL from 'url';
+import * as fs from 'fs';
+import C from '../common/constants';
+
+function download(
+  fileUrl: string,
+  distPath: string,
+  progress: (percent: string, bytes: number) => void,
+  callback: () => void,
+) {
+  return new Promise(
+    (resolve: (value?: any) => void, reject: (err: any) => void) => {
+      let timeout = 10000;
+
+      let file = fs.createWriteStream(distPath);
+
+      const parsedUrl = URL.parse(fileUrl);
+      let httpObject;
+      if (parsedUrl.protocol === 'https:') {
+        httpObject = https;
+      } else {
+        httpObject = http;
+      }
+
+      let request = httpObject.get(fileUrl).on('response', (res: any) => {
+        if (res.statusCode !== C.HTTP_OK) {
+          clearTimeout(timeoutId);
+          reject(new Error(res.statusMessage));
+          return;
+        }
+
+        let len = parseInt(res.headers['content-length'], 10);
+        let downloaded = 0;
+
+        res
+          .on('data', (chunk: any) => {
+            file.write(chunk);
+            downloaded += chunk.length;
+            // console.log('Downloading ' + (100.0 * downloaded / len).toFixed(2) + '% ' + downloaded + ' bytes');
+            progress(((100.0 * downloaded) / len).toFixed(0), downloaded);
+
+            // reset timeout
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(fn, timeout);
+          })
+          .on('end', () => {
+            // clear timeout
+            clearTimeout(timeoutId);
+            file.end();
+            console.log('downloaded to: ' + distPath);
+            resolve();
+            callback();
+          })
+          .on('error', (err: any) => {
+            // clear timeout
+            console.log('res.on(error): ' + err.message);
+            clearTimeout(timeoutId);
+            reject(err);
+          });
+      });
+
+      // error handler
+      request.on('error', (err: any) => {
+        console.log('request.on(error): ' + err.message);
+        clearTimeout(timeoutId);
+        reject(new Error(err.message));
+      });
+
+      // generate timeout handler
+      let fn = timeoutWrapper(request);
+
+      // set initial timeout
+      let timeoutId = setTimeout(fn, timeout);
+    },
+  );
+}
+
+function timeoutWrapper(req: any) {
+  return () => {
+    req.abort();
+  };
+}
+
+export default download;
