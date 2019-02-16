24b10e166fcfe7a9ab40f1ecd58488ecad98ff22
hisasann <hisasann25@gmail.com>
2019-02-16 18:23:28 +0900

    test(env): add env test

--- packages/lemon-sour/__tests__/env.dev.test.ts
+++ packages/lemon-sour/__tests__/env.dev.test.ts
@@ -1,7 +1,13 @@
-import Env from '../src/common/env.dev';
+process.env.NODE_ENV = 'dev';
+import EnvDev from '../src/common/env.dev';
+import Env from '../src/common/env';
 
 describe('check env.dev', () => {
   test('check env.dev envName is dev', () => {
+    expect(EnvDev.envName).toBe('dev');
+  });
+
+  test('should return env is dev', () => {
     expect(Env.envName).toBe('dev');
   });
 });
--- packages/lemon-sour/__tests__/env.prod.test.ts
+++ packages/lemon-sour/__tests__/env.prod.test.ts
@@ -1,7 +1,13 @@
-import Env from '../src/common/env.prod';
+process.env.NODE_ENV = 'prod';
+import EnvProd from '../src/common/env.prod';
+import Env from '../src/common/env';
 
 describe('check env.prod', () => {
   test('check env.prod envName is prod', () => {
+    expect(EnvProd.envName).toBe('prod');
+  });
+
+  test('should return env is prod', () => {
     expect(Env.envName).toBe('prod');
   });
 });
