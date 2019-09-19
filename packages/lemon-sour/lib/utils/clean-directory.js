bfa7301cee658d4b655621d8ace9915ac516fcff
hisamatsu yoshiyuki <hisasann25@gmail.com>
2019-09-20 05:15:50 +0900

    🥓 lintfix

--- packages/lemon-sour/lib/utils/clean-directory.js
+++ packages/lemon-sour/lib/utils/clean-directory.js
@@ -1,18 +1,19 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const rimraf = require("rimraf");
 const razer_1 = require("razer");
-const cleanDirectory = (directoryPath) => __awaiter(this, void 0, void 0, function* () {
+const cleanDirectory = (directoryPath) => __awaiter(void 0, void 0, void 0, function* () {
     razer_1.default('clean directory:', directoryPath + '/*');
-    return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+    return new Promise((resolve, reject) => __awaiter(void 0, void 0, void 0, function* () {
         // https://stackoverflow.com/questions/27072866/how-to-remove-all-files-from-directory-without-removing-directory-in-node-js
         rimraf(directoryPath + '/*', () => {
             resolve(true);
--- packages/lemon-sour/lib/utils/judgment-online.js
+++ packages/lemon-sour/lib/utils/judgment-online.js
@@ -1,9 +1,10 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -14,8 +15,8 @@ const constants_1 = require("../common/constants");
  * オフライン・オンラインの判定をする関数
  * @param env
  */
-const judgmentOnLine = (env) => __awaiter(this, void 0, void 0, function* () {
-    return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+const judgmentOnLine = (env) => __awaiter(void 0, void 0, void 0, function* () {
+    return new Promise((resolve, reject) => __awaiter(void 0, void 0, void 0, function* () {
         // オフラインかどうかの判定
         if (env.envName !== 'dev') {
             // https://stackoverflow.com/questions/5725430/http-test-server-accepting-get-post-requests
